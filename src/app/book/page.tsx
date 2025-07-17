5216e82bde2af7596d79e00c31d9a2e8dc4ebd54
Zheero0 <reeco.liburd@outlook.com>
2025-07-17 21:41:12 +0100

    bg

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -8,7 +8,7 @@ export default function BookPage() {
     <div className="bg-background text-foreground">
       <div className="fixed inset-0 z-0">
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
-        <div className="absolute left-1/2 top-0 h-[100vh] w-[100vw] -translate-x-1/2 bg-[radial-gradient(circle_50%_50%_at_50%_50%,#8EACFF33,transparent)]"></div>
+        <div className="absolute left-1/2 top-0 h-[100vh] w-[100vw] -translate-x-1/2 bg-[radial-gradient(circle_50%_50%_at_50%_50%,#8EACFF33,transparent)] backdrop-blur-[2px]"></div>
       </div>
       
       <div className="relative z-10 flex flex-col min-h-screen">
